e980d8bb382c9ddf09ac5024986f67c273d5adb2
wagyourtail <ruwagray@gmail.com>
2018-08-08 17:38:22 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const {app, BrowserWindow, Menu} = require('electron');
+const path = require('path');
+const url = require('url');
+const shell = require('electron').shell;
+const ipc = require('electron').ipcMain;
+
+let win;
+
+
+function createWindow() {
+    win  = new BrowserWindow({frame:false,width:1440, height:900});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'src/html/index.html'),
+        protocol:'file',
+        slashes: true
+    }));
+
+    win.webContents.openDevTools();
+
+    win.setFullScreen(true);
+    win.on('closed', () => {
+        win = null;
+    });
+    
+    /*
+    let menu = Menu.buildFromTemplate([{
+        label: 'Menu',
+        submenu: [
+            {label: 'Adjust Notification Value'},
+            {type:'separator'},
+            {
+                label: 'CoinMarketCap',
+                click() {
+                    shell.openExternal("http://coinmarketcap.com");
+                }
+            },
+            {type:'separator'},
+            {
+                label: 'Exit',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }])
+    Menu.setApplicationMenu(menu);
+    */
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
