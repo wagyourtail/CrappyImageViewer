80f1d934580b9840c8f726ed1adf9734ec667971
wagyourtail <ruwagray@gmail.com>
2018-08-14 03:02:05 -0600

    organizing and adding change folders

--- src/js/index.js
+++ src/js/index.js
@@ -2,48 +2,34 @@ const electron = require("electron");
 const imageSize = require('image-size');
 const fs = require("fs");
 const remote = electron.remote;
-const dir = remote.getGlobal("dir");
-const dirList = fs.readdirSync(dir);
+let dir = "";
 let directorys = [];
 let files = [];
-console.log(remote.BrowserWindow.isOffscreen);
+let currentImg = 0;
 
-
-document.getElementById("title").innerHTML = dir;
-
-dirList.forEach(x => {
-    if (fs.statSync(`${dir}/${x}`).isDirectory()) {
-        directorys.push(x);
-    } else {
-        files.push(x);
-    }
-});
-
-
-let loadIcons = new Worker("../js/loadIconsWorker.js");
-loadIcons.onmessage = (imageIcons) => {
-    imageIcons = imageIcons.data;
-    document.getElementById("imageIcons").innerHTML = imageIcons;
+const loadIcons = new Worker("../js/loadIconsWorker.js");
+loadIcons.onmessage = (icons) => {
+    document.getElementById("imageIcons").innerHTML = icons.data;
     scrollFn();
 }
-loadIcons.postMessage({dir:dir, files:files});
 
-
-
-
-let shuffle = () => {
+// ---------- FUNCTIONS ---------- //
+const shuffle = () => {
     for (let i = files.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [files[i], files[j]] = [files[j], files[i]];
         [document.getElementById(`image-${i}`).innerHTML, document.getElementById(`image-${j}`).innerHTML] = [document.getElementById(`image-${j}`).innerHTML.replace("src=", "alt="), document.getElementById(`image-${i}`).innerHTML.replace("src=", "alt=")];
+        if(i == currentImg) {
+            setImg(j);
+        } else if (j == currentImg) {
+            setImg(i);
+        }
     }
     scrollFn();
 }
 
 
-let currentImg = 0;
-
-let setImg = (num) => {
+const setImg = (num) => {
     let str = files[num];
     currentImg = num;
     imageSize(`${dir}/${str}`, (err,info) => {
@@ -58,6 +44,47 @@ let setImg = (num) => {
     });
 }
 
+const resizeFn = () => {
+    document.getElementById("currentImg").setAttribute("height", window.innerHeight);
+    document.getElementById("currentImg").setAttribute("width", window.innerWidth);
+}
+
+const scrollFn = () => {
+    let scrollDistance = imageIcons.scrollLeft;
+    let a = Math.floor((scrollDistance-10)/138);
+    for (let x = a-50; x < a+window.innerWidth/138+50; x++) {
+        if (x > -1 && x < files.length) {
+            if (Math.abs(a-x) < 25) {
+                document.getElementById(`image-${x}`).innerHTML = document.getElementById(`image-${x}`).innerHTML.replace("alt=", "src=");
+            } else {
+                document.getElementById(`image-${x}`).innerHTML = document.getElementById(`image-${x}`).innerHTML.replace("src=", "alt=");
+            }
+        }
+    }
+}
+
+let setFolder = (d) => {
+    dir = d;
+    files = [];
+    directorys = [];
+    let dirList = fs.readdirSync(dir);
+    
+    imageIcons.innerHTML = "";
+    title.innerHTML = dir;
+    dirList.forEach(x => {
+        if (fs.statSync(`${dir}/${x}`).isDirectory()) {
+            directorys.push(x);
+        } else {
+            files.push(x);
+        }
+    });
+
+    loadIcons.postMessage({dir:dir, files:files});
+    setImg(0);
+}
+
+// ----------- EVENTS ----------- //
+
 
 window.addEventListener("keydown", (key) => {
     if (key.key == "ArrowLeft" && currentImg > 0) {
@@ -70,51 +97,40 @@ window.addEventListener("keydown", (key) => {
 });
 
 
-setImg(0);
 
-
-document.getElementById("minBtn").addEventListener("click", () => {remote.BrowserWindow.getFocusedWindow().minimize()});
-document.getElementById("maxBtn").addEventListener("click", () => {
+minBtn.addEventListener("click", () => {remote.BrowserWindow.getFocusedWindow().minimize()});
+maxBtn.addEventListener("click", () => {
     let fullscreen = remote.BrowserWindow.getFocusedWindow().isFullScreen();
     remote.BrowserWindow.getFocusedWindow().setFullScreen(!fullscreen);
-    document.getElementById("topbar").setAttribute("style", fullscreen ? "height: 20px;" : "");
-    document.getElementById("title").setAttribute("style", fullscreen ? "-webkit-app-region: drag;" : "");
+    topbar.setAttribute("style", fullscreen ? "height: 20px;" : "");
+    title.setAttribute("style", fullscreen ? "-webkit-app-region: drag;" : "");
 });
-document.getElementById("closeBtn").addEventListener("click", window.close);
+closeBtn.addEventListener("click", window.close);
 
 
 
 
-document.getElementById("imageIcons").addEventListener("mousewheel", (e) => {
+imageIcons.addEventListener("mousewheel", (e) => {
     e = window.event || e;
     let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-    document.getElementById('imageIcons').scrollLeft += (delta*40); // Multiplied by 40
+    imageIcons.scrollLeft += (delta*40); // Multiplied by 40
     e.preventDefault();
 });
 
-document.getElementById("topbarContainer").addEventListener("mouseover", () => {
-    document.getElementById("imageIcons").scrollLeft = currentImg*138;
+topbar.addEventListener("mouseenter", () => {
+    imageIcons.scrollLeft = currentImg*138;
 });
 
-
-const resizeFn = () => {
-    document.getElementById("currentImg").setAttribute("height", window.innerHeight);
-    document.getElementById("currentImg").setAttribute("width", window.innerWidth);
-}
-
-const scrollFn = () => {
-    let scrollDistance = document.getElementById("imageIcons").scrollLeft;
-    let a = Math.floor((scrollDistance-10)/138);
-    for (let x = a-50; x < a+window.innerWidth/138+50; x++) {
-        if (x > -1) {
-            if (Math.abs(a-x) < 25) {
-                document.getElementById(`image-${x}`).innerHTML = document.getElementById(`image-${x}`).innerHTML.replace("alt=", "src=");
-            } else {
-                document.getElementById(`image-${x}`).innerHTML = document.getElementById(`image-${x}`).innerHTML.replace("src=", "alt=");
-            }
-        }
-    }
-}
 imageIcons.addEventListener("scroll", scrollFn);
 window.addEventListener("resize", resizeFn);
+
+title.addEventListener("click" , () => {
+    setFolder(remote.dialog.showOpenDialog(remote.BrowserWindow.getFocusedWindow(), {properties: ['openDirectory']})[0]);
+
+});
+
+// ----- MAIN CODE ----- //
+
+
+setFolder(remote.getGlobal("dir"));
 resizeFn();
